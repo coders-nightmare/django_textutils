82d688397db40d3867b188ca84697b9cd5375159
Ritik <ritik09052000@gmail.com>
2020-05-29 21:36:57 +0530

    Added new features to Text Analyzer

--- mysite2/views.py
+++ mysite2/views.py
@@ -2,23 +2,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Code for practice
-# def index(request):
-#     return HttpResponse('''<h1>Hello Ritik</h1>
-#     <br>
-#     <br>
-#     <a href="https://www.youtube.com/watch?v=AepgWsROO4k&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=7" alt="youtube">Youtube.com</a> ''')
-
-
-# def about(request):
-#     return HttpResponse("<h1>About Ritik</h1>")
-
-
-# def home(request):
-#     file = open("home.txt")
-#     return HttpResponse(file.readlines())
-#     file.close()
-
 
 def index(request):
     params = {'name': "Ritik", 'age': 20}
@@ -26,8 +9,14 @@ def index(request):
 
 
 def analyze(request):
+    # get text
     text = request.GET.get('text', None)
     removepunc = request.GET.get('removepunc', None)
+    fullcaps = request.GET.get('fullcaps', None)
+    newlineremover = request.GET.get('newlineremover', None)
+    spaceremover = request.GET.get('spaceremover', None)
+    charcount = request.GET.get('charcount', None)
+    # check for puctuation
     if removepunc == "on":
         punctuations = '''!"#$%&'()*+,; -./:<= >?@[\] ^ _`{ | }~'''
         analyzed_text = ""
@@ -35,6 +24,37 @@ def analyze(request):
             if char not in punctuations:
                 analyzed_text = analyzed_text+char
         d = {'analyzed_text': analyzed_text, 'purpose': 'removed puctuation'}
+        return render(request, 'analyze.html', d)
+
+    # check for capitalization
+    elif fullcaps == 'on':
+        analyzed = ""
+        for char in text:
+            analyzed = analyzed + char.upper()
+        d = {'analyzed_text': analyzed, 'purpose': 'Changed to UPPERCASE'}
+        return render(request, 'analyze.html', d)
+    # new line remover
+    elif newlineremover == 'on':
+        analyzed = ""
+        for char in text:
+            if char != '\n':
+                analyzed = analyzed + char
+        d = {'analyzed_text': analyzed, 'purpose': 'Removed new lines'}
+        return render(request, 'analyze.html', d)
+    # Space remover
+    elif spaceremover == 'on':
+        analyzed = ""
+        for index, char in enumerate(text):
+            if text[index] == ' ' and text[index+1] == ' ':
+                pass
+            else:
+                analyzed = analyzed + char
+        d = {'analyzed_text': analyzed, 'purpose': 'Removed extra Spaces'}
+        return render(request, 'analyze.html', d)
+    # Character counter
+    elif charcount == 'on':
+        analyzed = "No of Charaters = "+str(len(text))
+        d = {'analyzed_text': analyzed, 'purpose': 'Characters Counted'}
         return render(request, 'analyze.html', d)
     else:
         return HttpResponse("<h1>Error<h1>")
